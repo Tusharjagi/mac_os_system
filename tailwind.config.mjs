3364916a17ec3a12c2927e91f5848c52273e6067
Tusharjagi <tusharjagi@gmail.com>
2025-02-08 16:04:30 +0530

    cn created

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./components/**/*.{js,jsx,mdx}", "./app/**/*.{js,jsx,mdx}"],
   theme: {
     extend: {},
   },
